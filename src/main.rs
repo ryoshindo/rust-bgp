19192ab8c3470b872522267f3640f0cc4179ce5d
ryoshindo <udoryo999@gmail.com>
2023-01-11 00:59:43 +0900

    add 2.6 chapter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::str::FromStr;
+
+use rust_bgp::peer::Peer;
+use rust_bgp::config::Config;
+
+use tokio::time::{sleep, Duration};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let configs = vec![
+        Config::from_str("64512 127.0.0.1 65413 127.0.0.2 active").unwrap()];
+    let mut peers: Vec<Peer> = configs
+        .into_iter()
+        .map(Peer::new)
+        .collect();
+    for peer in &mut peers {
+        peer.start();
+    }
+
+    let mut handles = vec![];
+    for mut peer in peers {
+        let handle = tokio::spawn(async move {
+            loop {
+                peer.next().await;
+                sleep(Duration::from_secs_f32(0.1)).await;
+            }
+        });
+        handles.push(handle);
+    }
+
+    // main 関数が勝手に終了しないようにしている
+    for handle in handles {
+        handle.await;
+    }
 }
